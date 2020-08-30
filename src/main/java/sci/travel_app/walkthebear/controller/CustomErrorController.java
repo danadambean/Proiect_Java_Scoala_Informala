762f5c2ac04c1aaff2de2f6583214a4ee0965212
danadambean <daniela.dambean@yahoo.com>
2020-08-30 18:41:59 +0300

    rating

--- src/main/java/sci/travel_app/walkthebear/controller/CustomErrorController.java
+++ src/main/java/sci/travel_app/walkthebear/controller/CustomErrorController.java
@@ -1,4 +1,3 @@
-/*
 package sci.travel_app.walkthebear.controller;
 
 import org.springframework.boot.web.servlet.error.ErrorController;
@@ -18,4 +17,3 @@ public class CustomErrorController implements ErrorController {
         return "/error";
     }
 }
-*/
