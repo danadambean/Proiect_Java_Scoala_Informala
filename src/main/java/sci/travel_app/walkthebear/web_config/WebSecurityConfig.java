f91a583a1ca5d48a8174c916497b1c902004ae68
Valentino-Florin <57372860+Valentino-Florin@users.noreply.github.com>
2020-09-11 16:11:46 +0300

    Update WebSecurityConfig.java

--- src/main/java/sci/travel_app/walkthebear/web_config/WebSecurityConfig.java
+++ src/main/java/sci/travel_app/walkthebear/web_config/WebSecurityConfig.java
@@ -67,7 +67,7 @@ protected void configure(AuthenticationManagerBuilder auth) throws Exception {
                 .antMatchers("/addplace").hasAnyAuthority("Host","Admin")
                 .antMatchers("/editplace/**").hasAnyAuthority("Host","Admin")
                 .antMatchers("/addphotos/**").hasAnyAuthority("Host","Admin")
-                .antMatchers("/editprofile/**").hasAnyAuthority("Host", "Admin")
+                .antMatchers("/editprofile/**").hasAnyAuthority("Host", "Admin", "Traveler")
                 .antMatchers("/profileinfo").hasAnyAuthority("Host","Traveler")
                 .antMatchers("/profilefavorites").hasAuthority("Traveler")
                 .antMatchers("/profileratings").hasAuthority("Traveler")
